48967decb080163fe807c9c8d28e600e2c58428e
anonymous <nogolev6@gmail.com>
2024-09-05 12:18:21 +0300

    Стартовый проект готов, вынес создания приложения в отдельный модуль и добавил сторонние ссылки для загрузки статических файлов для документации

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,28 +1,15 @@
-from contextlib import asynccontextmanager
-from core.models import db_helper
 import uvicorn
-from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
-from fastapi.responses import ORJSONResponse
+from create_fastapi_app import create_app
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Запуск приложения
-    yield
-    # Остановка приложения
-    print("Завершение приложения... stopping server... Done!  :D")
-    await db_helper.dispose()  # Закрытие соединения с базой данных
-
-
-main_app = FastAPI(
-    default_response_class=ORJSONResponse,  # Использование ORJSONResponse для ускорения отправки JSON
-    lifespan=lifespan,
-)  # Инициализация FastAPI
-main_app.include_router(api_router)
+main_app = create_app(
+    create_custom_static_urls=True  # Создание статических роутеров документации (Swagger)
+)
+main_app.include_router(api_router)  # Регистрация роутера API в основном приложении
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:main_app", reload=True, host=settings.run.host, port=settings.run.port
-    )
+    )  # Запуск приложения на сервере
