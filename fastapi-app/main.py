19af82f61f933ff49116c982d0afc0519d2d123d
anonymous <nogolev6@gmail.com>
2024-09-05 11:30:09 +0300

    Добавляем ORJSONResponse и устанавливаем orjson для ускорения

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -4,6 +4,7 @@ import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
+from fastapi.responses import ORJSONResponse
 
 
 @asynccontextmanager
@@ -15,8 +16,11 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()  # Закрытие соединения с базой данных
 
 
-main_app = FastAPI(lifespan=lifespan)  # Инициализация FastAPI
-main_app.include_router(api_router, prefix=settings.api.prefix)
+main_app = FastAPI(
+    default_response_class=ORJSONResponse,  # Использование ORJSONResponse для ускорения отправки JSON
+    lifespan=lifespan,
+)  # Инициализация FastAPI
+main_app.include_router(api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
